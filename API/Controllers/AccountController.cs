b10c4005d538130b544b76e413c3d179dea04ec3
arshpreet33 <arshpreetsingh33@gmail.com>
2024-04-28 00:21:57 -0600

    Added Login endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using API.DTOs;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized("Invalid email");
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (!result) return Unauthorized();
+
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Token = "This will be a token",
+                Username = user.UserName
+            };
+        }
+    }
+}
