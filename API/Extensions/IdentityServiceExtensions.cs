4bfc9f5a5dc2bf6ebbe94e9b4ffcc67ec363dcf8
arshpreet33 <arshpreetsingh33@gmail.com>
2024-04-27 23:55:38 -0600

    Added AspNetCore Identity to Domain project & injected it to dependency injection

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<DataContext>();
+
+            services.AddAuthentication();
+
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddJwtBearer(opt =>
+            //    {
+            //        opt.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidateIssuerSigningKey = true,
+            //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+            //            ValidateIssuer = false,
+            //            ValidateAudience = false
+            //        };
+            //    });
+
+            return services;
+        }
+    }
+}
