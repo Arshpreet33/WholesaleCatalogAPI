fb4236dd73847950c968d0d62cf9e40b20779e99
arshpreet33 <arshpreetsingh33@gmail.com>
2024-05-19 23:51:33 -0600

    Added UsersController class

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+using Application.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class UsersController : BaseAPIController
+    {
+        [HttpGet]   //api/users
+        public async Task<IActionResult> GetUsers([FromQuery] UserParams param)
+        {
+            return HandlePagedResult(await Mediator.Send(new List.Query { Params = param }));
+        }
+
+        [HttpGet("{username}")]   //api/users/username
+        public async Task<IActionResult> GetUserByUsername(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { UserName = username }));
+        }
+
+        [HttpPost]   //api/users
+        public async Task<IActionResult> CreateUser(UserDto user)
+        {
+            return HandleResult(await Mediator.Send(new Create.Command { User = user }));
+        }
+
+        [HttpPut("{username}")]   //api/users/username
+        public async Task<IActionResult> EditUser(string username, UserDto user)
+        {
+            user.UserName = username;
+            return HandleResult(await Mediator.Send(new Edit.Command { User = user }));
+        }
+
+        [HttpPut("toggle/{username}")]   //api/users/toggle/username
+        public async Task<IActionResult> ToggleActiveStatus(string username)
+        {
+            return HandleResult(await Mediator.Send(new ToggleActive.Command { UserName = username }));
+        }
+    }
+}
